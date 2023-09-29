73c4e9298354fd6d81267545b8c2b9f4e898e8ef
Jack O'Caoimh <jackocaoimh@gmail.com>
2023-09-29 17:50:30 +0100

    fully functioning

--- script.js
+++ script.js
@@ -3,26 +3,42 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
-}
+input.addEventListener('keyup', searchHandler);
 
 function searchHandler(e) {
-	// TODO
+	let searchVal = e.target.value;
+	search(searchVal);
 }
 
-function showSuggestions(results, inputVal) {
-
-	// TODO
+function search(str) {
+	let results = [];
+	fruit.forEach((item) => {
+		// if there is a fruit(s) that includes str, append matching fruit to results array 
+		if(item.toLowerCase().includes(str.toLowerCase())){
+			results.push(item);
+		}
+	});
+	showSuggestions(results);
 }
+
+function showSuggestions(results) {
+	// prevents entire array appearing 
+	suggestions.innerHTML = '';
+	results.forEach((result) => {
+		const li = document.createElement('li');
+		li.textContent = result;
+		suggestions.appendChild(li);
+	});
+}
+
+suggestions.addEventListener('click', useSuggestion);
 
 function useSuggestion(e) {
-	// TODO
+	let suggestion = e.target.textContent;
+	input.value = suggestion;
 }
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+
+
+
+
